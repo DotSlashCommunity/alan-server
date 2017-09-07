e400c4d84f88d1c8752ee09cc448cb88e580ec23
Kilari Teja <kilariteja9@gmail.com>
2017-09-07 15:45:13 +0530

    fixed scores repr to conserve order while as json

--- alan/abstract/scores.py
+++ alan/abstract/scores.py
@@ -13,6 +13,9 @@ def getScores(rmodel):
 			lambda l: 0 if l < 0 else l,
 			player.replies.values()))
 
-	return dict(sorted(
+	# send a list of lists,
+	# dict may rearrange, list
+	# stays ordered!
+	return map(list, (sorted(
 		scores.iteritems(),
-		key=lambda l: l[1]))
+		key=lambda l: l[1])))
